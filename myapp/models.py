da1ef817fbef5a87578d11ad24e2740128aede4f
xiaoxx-mac <me@xiaoxx.cc>
2022-04-17 20:53:12 +0800

    try1

--- myapp/models.py
+++ myapp/models.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='uploads/%Y/%m/%d')
+class Results(models.Model):
+    docfile = models.FileField(upload_to='results/%Y/%m/%d')
