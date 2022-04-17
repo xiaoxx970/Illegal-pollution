a76e6e54cb301a893b7b3465f45345b873bc050f
xiaoxx-mac <me@xiaoxx.cc>
2022-04-17 21:10:02 +0800

    done

--- myapp/views.py
+++ myapp/views.py
@@ -7,31 +7,36 @@ from .forms import DocumentForm
 def my_view(request):
     message = '上传企业排污数据后点击上方链接下载结果'
     # Handle file upload
+    outurl = ""
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             newdoc = Document(docfile=request.FILES['docfile'])
             newdoc.save()
-
-            result = Results().objects.all(docfile="ressult.csv")
-
-            print(f'-i {newdoc.docfile.path} -o {result.docfile.path}')
-            print(result.docfiel.url)
+            documents = Document.objects.all()
+            filepath = documents[0].docfile.path
+            url = documents[0].docfile.url
+            outpath = "/".join(filepath.split("/")[:-1]) + "/ressult.csv"
+            outurl = "/".join(url.split("/")[:-1]) + "/ressult.csv"
+            print(f'-i {filepath} -o {outpath}')
+            print(outurl)
             try:
-                code = os.system(f'python kmeans_calc.py -i {newdoc.docfile.path} -o {newdoc.docfile.path}')
+                code = os.system(f'python kmeans_calc.py -i {filepath} -o {outpath}')
             except Exception as e:
                 message = str(e)
             if code != 0:
                 message = f'Exec script failed, code: {code}'
 
             # Redirect to the document list after POST
-            return redirect('my-view')
+            # return redirect('my-view')
+            context = {'outurl': outurl, 'form': form, 'message': message}
+            return render(request, 'list.html', context)
         else:
             message = 'The form is not valid. Fix the following error:'
     else:
         form = DocumentForm()  # An empty, unbound form
 
-    result = Results().objects.all()[0]
+
     # Render list page with the documents and the form
-    context = {'result': result, 'form': form, 'message': message}
+    context = {'outurl': outurl, 'form': form, 'message': message}
     return render(request, 'list.html', context)
