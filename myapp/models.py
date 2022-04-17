3af0d2cb1b7561825afd72286e3d9a73371ade88
xiaoxx-mac <me@xiaoxx.cc>
2022-04-17 21:05:59 +0800

    fix models

--- myapp/models.py
+++ myapp/models.py
@@ -3,5 +3,3 @@ from django.db import models
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='uploads/%Y/%m/%d')
-class Results(models.Model):
-    docfile = models.FileField(upload_to='results/%Y/%m/%d')
